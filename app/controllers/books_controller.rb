e5f5ab39843380e399bf60b1adb207faea9e94e3
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2022-10-03 06:00:58 +0000

    [Add]3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,12 @@ class BooksController < ApplicationController
   def new
   end
 
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/top'
+  end
+
   def index
   end
 
@@ -9,5 +15,11 @@ class BooksController < ApplicationController
   end
 
   def edit
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title,:body)
   end
 end
