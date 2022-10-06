070c7acce44c1eeb5bd5adb2182c31f6e946637f
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2022-10-06 12:05:06 +0000

    [Add]3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,7 +5,8 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/top'
+    flash[:notice] = "Book was successfully created."
+    redirect_to book_path(book.id)
   end
 
   def index
@@ -14,6 +15,7 @@ class BooksController < ApplicationController
 
   def show
     @book=Book.find(params[:id])
+    
   end
 
   def edit
