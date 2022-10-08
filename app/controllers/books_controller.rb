904a56937e1b996981f4d5a1882fe1e93a9dc18f
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2022-10-08 17:30:57 +0000

    [Add]3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,20 +20,20 @@ class BooksController < ApplicationController
 
   def show
     @book=Book.find(params[:id])
-    
+
   end
 
   def edit
     @book=Book.find(params[:id])
   end
-  
+
   def update
     book=Book.find(params[:id])
     book.update(book_params)
     flash[:notice] = "Book was successfully updated."
     redirect_to book_path(book.id)
   end
-  
+
   def destroy
     book=Book.find(params[:id])
     book.destroy
